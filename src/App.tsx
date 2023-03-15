727eec26c0b99e425a648bef84b9fdfe94394d75
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-03-15 00:33:09 -0300

    feat: implement new fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { ThemeProvider } from "styled-components";
 import theme from "./Theme/theme";
+import GlobalFonts from './Fonts/fonts';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
+			<GlobalFonts />
 			<div>react app</div>
 		</ThemeProvider>
 	);
---
+++ src/Fonts/fonts.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+import AntonioWoff from './Antonio.woff';
+import AntonioWoff2 from './Antonio.woff2';
+import SpartanWoff from './Spartan.woff';
+import SpartanWoff2 from './Spartan.woff2';
+
+export default createGlobalStyle`
+	@font-face {
+			font-family: 'Antonio';
+			src: local('Antonio'), local('Antonio'),
+			url(${AntonioWoff}) format('woff');
+	}
+
+	@font-face {
+		font-family: 'Antonio';
+		src: local('Antonio'), local('Antonio'),
+		url(${AntonioWoff2}) format('woff2'),
+	}
+
+	@font-face {
+		font-family: 'Spartan';
+		src: local('Spartan'), local('Spartan'),
+		url(${SpartanWoff}) format('woff');
+	}
+
+	@font-face {
+		font-family: 'Spartan';
+		src: local('Spartan'), local('Spartan'),
+		url(${SpartanWoff2}) format('woff2'),
+	}
+
+	body {
+		margin: 0;
+		font-family: 'Antonio', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		}
+`;
