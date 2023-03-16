bc24cdfcd8f3424d0dd8abfd779cc0bf03557f86
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-03-15 22:41:30 -0300

    feat: implement basic code for header

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const MenuList = styled.ul`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 32px;
+	padding: 0;
+
+	& > li {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+`;
+
+export const Header = styled.header`
+	height: 70px;
+	width: 100%;
+	box-sizing: border-box;
+	padding: 0 32px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: ${(props) => props.theme.background};
+	color: ${(props) => props.theme.white};
+	border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as css from "./Header.styles";
+
+const Header: React.FC = () => {
+	return (
+		<css.Header>
+			<css.MenuList>
+				<li>ssss</li>
+				<li>ssss</li>
+				<li>ssss</li>
+			</css.MenuList>
+		</css.Header>
+	);
+};
+
+export default Header;
