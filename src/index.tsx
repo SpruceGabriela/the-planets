c7f7489c2588614fdb320b0560e61cbf7d8f2287
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-03-17 23:48:11 -0300

    feat: implement basic routing config

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+]);
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage: React.FC = () => {
+	const error = useRouteError();
+	console.error(error);
+
+	if (isRouteErrorResponse(error)) {
+		return (
+			<div id="error-page">
+				<h1>Oops!</h1>
+				<p>Sorry, an unexpected error has occurred.</p>
+				<p>
+					<i>{error.statusText}</i>
+				</p>
+			</div>
+		);
+	}
+	return (
+		<div id="error-page">
+			<h1>Oops!</h1>
+			<p>Sorry, an unexpected error has occurred.</p>
+		</div>
+	);
+};
+
+export default ErrorPage;
